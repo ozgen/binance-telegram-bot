2f4d6d02536b416b3fd0992d75cc2b1205214904
ozgen <ozgn45@gmail.com>
2024-01-03 09:03:53 +0300

    update trading signal parser

--- src/main/java/com/ozgen/telegrambinancebot/utils/parser/SignalParser.java
+++ src/main/java/com/ozgen/telegrambinancebot/utils/parser/SignalParser.java
@@ -9,23 +9,27 @@ import java.util.regex.Pattern;
 
 public class SignalParser {
 
-    private static final String PATTERN = "^(\\w+BTC)\\s*\\n\\nENTRY:\\s*(\\d+\\.\\d+)\\s*-\\s*(\\d+\\.\\d+)\\s*\\n\\n(TP\\d+:\\s*\\d+\\.\\d+\\s*)+\\n\\nSTOP:\\s*Close weekly below\\s*(\\d+\\.\\d+)\\s*\\n\\nSHARED:";
+    private static final String PATTERN = "(\\w+BTC)\\s*ENTRY:\\s*(\\d+\\.\\d+)\\s*-\\s*(\\d+\\.\\d+)((?:\\s*TP\\d+:\\s*\\d+\\.\\d+)+)\\s*STOP:\\s*Close weekly\\s*below\\s*(\\d+\\.\\d+)";
     private static final String TP_PATTERN = "(TP\\d+:\\s*(\\d+\\.\\d+))";
 
     public static TradingSignal parseSignal(String signalText) {
         TradingSignal tradingSignal = new TradingSignal();
 
+        // Remove empty lines
+        String condensedText = signalText.replaceAll("(?m)^[ \t]*\r?\n", "");
+
+        // Main pattern matcher
         Pattern pattern = Pattern.compile(PATTERN, Pattern.MULTILINE);
-        Matcher matcher = pattern.matcher(signalText);
+        Matcher matcher = pattern.matcher(condensedText);
 
         if (matcher.find()) {
             tradingSignal.setSymbol(matcher.group(1));
             tradingSignal.setEntryStart(matcher.group(2));
             tradingSignal.setEntryEnd(matcher.group(3));
 
+            // Parsing take profit values
             List<String> takeProfits = new ArrayList<>();
-            Pattern tpPattern = Pattern.compile(TP_PATTERN);
-            Matcher tpMatcher = tpPattern.matcher(signalText);
+            Matcher tpMatcher = Pattern.compile(TP_PATTERN).matcher(condensedText);
             while (tpMatcher.find()) {
                 takeProfits.add(tpMatcher.group(2));
             }
